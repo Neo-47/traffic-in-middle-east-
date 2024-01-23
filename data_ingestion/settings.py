2f5373d9db5251483712970d13c7d6d00a28d738
Neo-47 <m.elsersy96@gmail.com>
2024-01-23 19:57:48 +0000

    First commit

--- data_ingestion/settings.py
+++ data_ingestion/settings.py
@@ -10,3 +10,4 @@ PATH = f"data"
 DATASET_NAME = f"traffic-index-in-saudi-arabia-and-middle-east"
 DATASET_FILE = f"traffic_index"
 DATASET_URL = f"https://www.kaggle.com/datasets/majedalhulayel/traffic-index-in-saudi-arabia-and-middle-east?select=traffic_index.csv"
+ 
